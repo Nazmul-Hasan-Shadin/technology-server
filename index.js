41e4a5202e5b54438c22ca226a7b70b646e4ed95
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-10-18 10:03:41 +0600

    technology-electronics server has initialized

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app= express();
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+technology-electronics
+skn9HUpRVj8rmjnQ
+        //   mongodbConnection
