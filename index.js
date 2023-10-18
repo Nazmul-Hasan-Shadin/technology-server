47a8aa4b49af06034bbf37075e2d969b0ea447db
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-10-18 19:45:24 +0600

    update functionality and its route added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app= express();
 const port=process.env.PORT || 5000;
@@ -42,7 +42,7 @@ async function run() {
 //     find only brand related products
 
 app.get('/products/:id',async(req,res)=>{
-    const id=(req.params.id)
+    const id=req.params.id
     console.log(id);
     const result = await database.find({brand: id}).toArray()
     res.send(result)
@@ -56,7 +56,41 @@ app.get('/products/:id',async(req,res)=>{
      const result=await database.insertOne(data)
      res.send(result)
 
-   })            
+   })       
+   
+//    only single product load by get for update
+ app.get('/singleproducts/:id',async(req,res)=>{
+    const id= req.params.id
+      const query= {_id:new ObjectId(id)}
+       const result= await database.findOne(query)
+       res.send(result)
+ })
+
+//    single product load and update
+
+app.put('/update/:id',async(req,res)=>{
+    
+    const data= req.body;
+     const id= req.params.id;
+     const options = { upsert: true };
+     const filter= {_id: new ObjectId(id)}
+    const updateData= {
+        $set:{
+            productName:data.productName,
+            brand:data.brand,
+            type: data.type,
+            rating: data.rating,
+            price: data.price,
+            description: data.description,
+            imageUrl: data.imageUrl
+        }
+    }
+    const result = await database.updateOne(filter , updateData,options)
+    console.log(result);
+    res.send(result)
+    
+  
+})
    
 
 
